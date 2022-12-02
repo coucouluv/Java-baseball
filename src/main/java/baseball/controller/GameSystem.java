33b3272baebe2192626b346f56d7fb0a71b3af4e
coucouluv <oy5325@gmail.com>
2022-12-03 03:24:20 +0900

    feat: 숫자 입력 기능 구현

---
+++ src/main/java/baseball/controller/GameSystem.java
@@ -0,0 +1,17 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.view.InputView;
+
+public class GameSystem {
+    private Computer computer = new Computer();
+
+    public void run() {
+        //게임 시작 문구 출력
+        //게임 진행
+        //컴퓨터 수 세팅
+        //사용자 입력값 입력
+        //게임 결과 판별
+        //게임 재시작/종료 여부
+    }
+}
---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,28 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    //private static String input;
+    public static void startMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static String inputMessage() {
+        System.out.printf("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return input;
+    }
+
+    /**
+     * 여기서 매개변수 필요
+     */
+    public static void resultMessage() {
+
+    }
+    public static String restartOrEndMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine(); //공백 전까지 입력받음
+        return input;
+    }
+
+}
