4a7459273d4f4743bca8a0c6fc42132e1fa89e7d
coucouluv <oy5325@gmail.com>
2022-12-04 00:17:52 +0900

    feat: 사용자가 입력한 문자열을 리스트로 변경하는 기능 구현

--- src/main/java/baseball/controller/GameSystem.java
+++ src/main/java/baseball/controller/GameSystem.java
@@ -1,17 +1,26 @@
 package baseball.controller;
 
 import baseball.domain.Computer;
+import baseball.domain.User;
 import baseball.view.InputView;
 
 public class GameSystem {
     private Computer computer = new Computer();
-
+    private User user = new User();
     public void run() {
         //게임 시작 문구 출력
+        InputView.startMessage();
         //게임 진행
+        play();
         //컴퓨터 수 세팅
         //사용자 입력값 입력
         //게임 결과 판별
         //게임 재시작/종료 여부
+    }
+    public void play() {
+        computer.createNumber();
+        user.createNumber();
+        //게임 결과는??
+
     }
 }
---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import baseball.view.InputView;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class User implements Player{
+
+    private List<Integer> number;
+
+    @Override
+    public void createNumber() {
+        String input = InputView.inputMessage();
+        //예외 처리
+        number = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+    }
+    public List<Integer> getNumber() {
+        return number;
+    }
+}
